9ecdf85b02b2fd9b6208f178dd64cf8de53cdc17
TheOGAltruist <staraknath@yahoo.co.in>
2024-09-25 01:58:36 +0000

    added basic code for rendering puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { puppyList } from './data';
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+  
   return (
     <>
-      
+      {
+        puppyList.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
